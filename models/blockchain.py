2d5c8b8d13a6742b4641fc1039b17aed38b8593e
Liam Ellis <liamhellis@gmail.com>
2017-12-28 20:07:44 -0500

    cors and consensus algorithm

--- models/blockchain.py
+++ models/blockchain.py
@@ -56,6 +56,59 @@ class Blockchain(object):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
+    def valid_chain(self, chain):
+        # Determine if a given blockchain is valid using a consensus algorithm
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block=chain[current_index]
+
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the proof of work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+            return True
+
+        def resolve_conflicts(self):
+            # This here is the actual consensus algorithm
+
+            neighbors = self.nodes
+            new_chain = None
+
+            #We're only looking for chains longer than ours
+            max_length = len(self.chain)
+
+            # Grab and verify all the chains from all the nodes on our network
+            for node in neighbors:
+                response = requests.get(f'http://{node}/chain')
+
+                if response.status_code == 200:
+                    length = response.json()['length']
+                    chain = response.json()['chain']
+
+                    # Then we check if the length is longer and the chain is valid
+                    if length > max_length and self.valid_chain(chain):
+                        max_length = length
+                        new_chain = chain
+            
+            if new_chain:
+                self.chain = new_chain
+                return True
+
+            return False
+
 
     @staticmethod
     def valid_proof(last_proof, proof):
