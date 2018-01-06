725f888c2884e3114d3211825fb77e2c388b661b
Liam Ellis <liamhellis@gmail.com>
2018-01-06 15:56:18 -0500

    changes in data

--- models/album.py
+++ models/album.py
@@ -8,16 +8,14 @@ class Album(db.Model):
     name = db.Column(db.Text)
     artist_id = db.Column(db.Integer, db.ForeignKey(
         'artists.id'), primary_key=True)
-    songs = db.relationship('Song', backref='album', lazy='joined')
     created_at = db.Column(db.DateTime, default=datetime.now())
     photo = db.Column(db.Text, default=None)
     claps = db.Column(db.Integer, default=None)
     shares = db.Column(db.Integer, default=None)
     value = db.Column(db.Integer, default=None)
 
-    def __init__(self, id, name, artist_id, created_at, photo, claps, shares, value, songs):
+    def __init__(self, id, name, artist_id, created_at, photo, claps, shares, value):
         self.id = id
-        self.songs = songs
         self.name = name
         self.artist_id = artist_id
         self.created_at = created_at
