34602a2fecc46e02ad12f4cefb61a1034041c75e
Liam Ellis <liamhellis@gmail.com>
2018-01-13 20:14:53 -0500

    staging config database url

--- app/config.py
+++ app/config.py
@@ -18,6 +18,7 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
